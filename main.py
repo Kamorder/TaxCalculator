d04cc4b7f1fb04f2b85d79472e7df88a1babc4b3
Kevin Parfien <kparfien@uci.edu>
2022-12-14 19:33:04 -0600

    Main Written
    Wrote the main.py which is the primary driver of the small program which essentially takes a formatted file, and adds all the values together underneath a header then prints out the results of all the headers.

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from fileReader import inputFile, openFile
+from tax import taxFormat
 def main():
-    pass
+    initiateFile = inputFile("Please input the name of the formatted File: ")
+    taxDoc = taxFormat(openFile(initiateFile))
+    taxDoc.formatGen()
+    taxDoc.printResults()
+    taxDoc.writeInFile()
