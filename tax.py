6bf8a60dcd0bba2a9517b2015e1d4b6ef30b9f71
Kevin Parfien <kparfien@uci.edu>
2022-12-23 09:03:20 -0600

    formatGen and DuplicateLabel Error
    Finished formatGen and the duplicateLabel error, made it so that formatGen would add the split lines as a float and assign it to the current header. Also made it so the str dunder gave back a string containing the error.

--- tax.py
+++ tax.py
@@ -1,34 +1,40 @@
 class taxFormat:
     def __init__(self, gen):
-        self.taxLabels = dict()
+        self.taxHeader = dict()
         self.fileGen = gen
 
     def formatGen(self) -> None:
         try:
+            currentHeader = ''
             for line in self.fileGen:
                 if not line.split()[0].isnumeric():
-                    if line.strip() not in self.taxLabels:
-                        self.taxLabels[line.strip()] = []
+                    if line.strip() not in self.taxHeader:
+                        self.taxHeader[line.strip()] = 0
+                        currentHeader = line.strip()
                     else:
-                        pass
-        except:
-            pass
+                        raise duplicateLabel(line.strip())
+                else:
+                    if currentHeader:
+                        self.taxHeader[currentHeader] += sum((float(x) for x in line.split()))
 
+        except duplicateLabel as error:
+            print(error)
 
     def printResults(self) -> None:
-        for header, money in self.taxLabels.items():
+        for header, money in self.taxHeader.items():
             print(f'{header}: ${money:,.2f}')
 
     def writeInFile(self) -> None:
         fileName = input('Please type a file name: ')
         with open(fileName, "w") as newFile:
-            for header, money in self.taxLabels.items():
+            for header, money in self.taxHeader.items():
                 newFile.write(f'{header}: ${money:,.2f}\n')
         print(f"Done writing to File {fileName}")
 
 
 class duplicateLabel(Exception):
-    def __init__(self):
-        pass
-    def __str__(self):
-        pass
+    def __init__(self, dupeName):
+        self.dupeName = dupeName
+
+    def __str__(self) -> str:
+        return f'Error: The header -> {self.dupeName} was used as a header more than once'
