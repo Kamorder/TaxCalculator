30029366abbc2aaa95415cac3c88b8288b07e176
Kevin Parfien <107777411+Kamorder@users.noreply.github.com>
2023-09-29 05:49:19 -0500

    Added the 3 files necessary for the project

---
+++ main.py
@@ -0,0 +1,11 @@
+from fileReader import inputFile, openFile
+from tax import taxFormat
+def main():
+    initiateFile = inputFile("Please input the name of the formatted File: ")
+    taxDoc = taxFormat(openFile(initiateFile))
+    taxDoc.formatGen()
+    taxDoc.printResults()
+    taxDoc.writeInFile()
+
+if __name__ == "__main__":
+    main()
