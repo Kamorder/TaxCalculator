f9c0d6c6d2f6e09926a527a4f39af4f6de2b8713
Kevin Parfien <107777411+Kamorder@users.noreply.github.com>
2023-10-05 00:01:12 -0700

    Added a docstring to the main function.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fileReader import inputFile, openFile
 from tax import taxFormat
 def main():
+    '''Gets the file, initiates a taxFormat instance,and utilizes the methods to create the formatted Report.'''
     initiateFile = inputFile("Please input the name of the formatted File: ")
     taxDoc = taxFormat(openFile(initiateFile))
     taxDoc.formatGen()
