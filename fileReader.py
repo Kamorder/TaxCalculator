c7bad800c34badd6b8d2aeb92bc0c8f843216ba4
Kevin Parfien <107777411+Kamorder@users.noreply.github.com>
2023-10-04 23:48:58 -0700

    Added docstrings for the functions in fileReader

--- fileReader.py
+++ fileReader.py
@@ -3,11 +3,14 @@ from typing import Generator
 
 
 def inputFile(textStr = '') -> Path:
+    '''Turns a string into a file path and returns it'''
     return Path(input(textStr))
 
 def openFile(path) -> Generator:
+    '''Opens the file and returns a generator'''
     with open(path) as file:
         return yieldIter(file.readlines())
 
 def yieldIter(iterable) -> Generator:
+    '''turns an iterable into a generator'''
     yield from iterable
