4dc5021f448c301816bc77127c96db9387e6bd0f
Kevin Parfien <kparfien@uci.edu>
2024-08-14 16:07:33 -0700

    refactoring changes.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fileReader import inputFile, openFile
-from tax import taxFormat
+from utils.fileReader import inputFile, openFile
+from tax.tax import taxFormat
 def main():
     initiateFile = inputFile("Please input the name of the formatted File: ")
     taxDoc = taxFormat(openFile(initiateFile))
